bc5bfdadb018f62f90ff23c7e1176cdbd1a03e36
Nikolas Alexi <nikolas010760@gmail.com>
2024-07-03 15:09:53 -0300

    Update TesteGitEclipse.java

--- IntegracaoGitEclipse/src/gitTeste/TesteGitEclipse.java
+++ IntegracaoGitEclipse/src/gitTeste/TesteGitEclipse.java
@@ -5,7 +5,7 @@ public class TesteGitEclipse {
 	public static void main(String[] args) {
 
 		System.out.println("Integração Git Eclipse Primeiro Teste tentando.	");
-		
+		System.out.println("Alterando o código git.");
 	}
 
 }
