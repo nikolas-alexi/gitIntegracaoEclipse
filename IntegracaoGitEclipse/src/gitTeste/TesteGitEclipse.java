88f7e38fb935fc92e5db731061eabb73038cecf3
nikolas-alexi <nikolas010760@gmail.com>
2024-07-03 14:44:49 -0300

    commit inicial para o github

---
+++ IntegracaoGitEclipse/src/gitTeste/TesteGitEclipse.java
@@ -0,0 +1,11 @@
+package gitTeste;
+
+public class TesteGitEclipse {
+
+	public static void main(String[] args) {
+
+		System.out.println("Integração Git Eclipse Primeiro Teste tentando.	");
+		
+	}
+
+}
